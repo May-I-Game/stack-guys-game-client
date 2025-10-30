f903d63921179c999d7d91ec5a5708094333b8ec
Dohooyi <59619810+sisyphusman@users.noreply.github.com>
2025-10-30 14:55:24 +0900

    feat: Connect 버튼 누를 시 씬 전환하면서 Player Name 적용

---
+++ Assets/Scripts/NameTag.cs
@@ -0,0 +1,28 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NameTag : MonoBehaviour
+{
+    [SerializeField]
+    TMP_Text label;
+
+    [SerializeField]
+    Vector3 worldOffset = new Vector3(0, 2.0f, 0);
+
+    private void Awake()
+    {
+        string playerName = PlayerPrefs.GetString("player_name", "Player");
+
+        if (label)
+            label.text = playerName;
+    }
+
+    private void LateUpdate()
+    {
+        // 항상 카메라를 보게 함
+        if (Camera.main)
+            transform.LookAt(transform.position + Camera.main.transform.rotation * Vector3.forward,
+                Camera.main.transform.rotation * Vector3.up);
+    }
+}
