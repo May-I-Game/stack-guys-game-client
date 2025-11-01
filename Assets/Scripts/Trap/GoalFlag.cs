c53753981f521626b159ea07c784fdb1bcdd51f4
sjk2915 <102575943+sjk2915@users.noreply.github.com>
2025-11-01 13:34:14 +0900

    feat: 게임 종료 로직 서버 동기화

--- Assets/Scripts/Trap/GoalFlag.cs
+++ Assets/Scripts/Trap/GoalFlag.cs
@@ -2,24 +2,20 @@ using UnityEngine;
 
 public class GoalFlag : MonoBehaviour
 {
+    private bool hasFinished = false;
+
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player") && !GameManager.instance.gameEnded)
-        {
-            string playerName = PlayerPrefs.GetString("player_name", "Player");
+        // 태그 및 게임상태 확인
+        if (hasFinished || !other.CompareTag("Player") || GameManager.instance.gameEnded.Value) return;
 
-            DisablePlayer(other.gameObject);
+        // 플레이어 여부 및 내거인지 확인
+        var player = other.GetComponent<PlayerController>();
+        if (player == null || !player.IsOwner) return;
 
-            GameManager.instance.PlayerReachedGoal(playerName);
-        }
-    }
+        player.enabled = false;
 
-    private void DisablePlayer(GameObject player)
-    {
-        var controller = player.GetComponent<PlayerController>();
-        if (controller != null)
-        {
-            controller.enabled = false;
-        }
+        string playerName = PlayerPrefs.GetString("player_name", "Player");
+        GameManager.instance.PlayerReachedGoalServerRpc(playerName);
     }
 }
