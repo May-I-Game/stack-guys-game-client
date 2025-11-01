a5da07274450d6f6b8832d744c3a2f50a3aeb64e
kkc1383 <kangkc09@gmail.com>
2025-11-01 13:57:05 +0900

    feat : 웹에서도 동작하는지 확인
    코드 리펙토링 해야할 수도 있음

--- Assets/Scripts/LoginUI.cs
+++ Assets/Scripts/LoginUI.cs
@@ -25,12 +25,17 @@ public class LoginUI : MonoBehaviour
 
         // 선택한 캐릭터 인덱스 가져오기
         int selectedCharacterIndex = PlayerPrefs.GetInt("selected_character", 0);
+        Debug.Log($"선택한 캐릭터 인덱스 :{selectedCharacterIndex}");
 
         // 서버 연결
+#if UNITY_EDITOR
         UnityTransport transport = NetworkManager.Singleton.GetComponent<UnityTransport>();
         string serverAddress = transport.ConnectionData.Address;
         ushort serverPort = transport.ConnectionData.Port;
-
+#else
+        const string serverAddress = "54.180.159.66";  // ← EC2 공인 IP로 교체 가능
+        const ushort serverPort = 7779;
+#endif
         Debug.Log($"Connecting to {serverAddress}:{serverPort}...");
 
         if (NetworkManager.Singleton != null)
