ac99097d4a09bfe6bd448124b7a5c26d680b7d01
이정호 <117905423+JGLeejungHo@users.noreply.github.com>
2025-11-10 15:15:53 +0900

    Revert "fix: loading"

--- Assets/Scripts/Server/ServerOptimizer.cs
+++ Assets/Scripts/Server/ServerOptimizer.cs
@@ -10,15 +10,9 @@ public static class ServerOptimizer
     [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
     private static void Initialize()
     {
-        // 추가 안전장치: 에디터에서는 실행하지 않음
-        if (Application.isEditor)
-        {
-            Debug.Log("[ServerOptimizer] Skipped in Editor");
-            return;
-        }
-
         OptimizeFrameRate();
         OptimizeRendering();
+
         Debug.Log("[ServerOptimizer] Server optimizations applied");
     }
 
@@ -41,11 +35,6 @@ public static class ServerOptimizer
     /// </summary>
     private static void OptimizeRendering()
     {
-        if (Application.isEditor)
-        {
-            Debug.Log("[ServerOptimizer] Skipped in Editor");
-            return;
-        }
         // 카메라 비활성화 (Headless 서버에서 불필요)
         DisableCameras();
 
